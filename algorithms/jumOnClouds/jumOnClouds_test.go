5fab03e51bbdde451d775a2a8fdddb5404a0a7a9
krystianlewandowski <lewy9109@gmail.com>
2023-06-11 13:12:24 +0200

    Add tests

--- algorithms/jumOnClouds/jumOnClouds_test.go
+++ algorithms/jumOnClouds/jumOnClouds_test.go
@@ -12,3 +12,25 @@ func TestJumpingOnClouds(t *testing.T) {
 		t.Errorf("got %q, wanted %q", result, want)
 	}
 }
+
+func TestJumpOnCloudsSecondTest(t *testing.T) {
+	var array [7]int32 = [7]int32{0,0,1,0,0,1,0}
+
+	var want int32 = 4
+	result := JumpingOnClouds(array[:])
+
+	if want != result {
+		t.Errorf("got %q, wanted %q", result, want)
+	}
+}
+
+func TestJumpOnCloudOnlyZero(t *testing.T) {
+	var array [7]int32 = [7]int32{0,0,0,0,0,0,0}
+
+	var want int32 = 3
+	result := JumpingOnClouds(array[:])
+
+	if want != result {
+		t.Errorf("got %q, wanted %q", result, want)
+	}
+}
