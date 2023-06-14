73910d3133ef58bc6cf71f54d9b55bf380660e85
krystianlewandowski <lewy9109@gmail.com>
2023-06-14 20:38:59 +0200

    add function to find repeted first letter in string, and add tests

--- algorithms/repeatedString/repeatedString.go
+++ algorithms/repeatedString/repeatedString.go
@@ -8,22 +8,15 @@ func RepeatedStirngs(s string, n int64) int64 {
 		return n
 	}
 
-	// 1. Mamy nieskonczony ciag znakow np abca
-
-	// 2  n jest liczba gdze trzeba znaleźć ile razy sie powtaza pierwsza litera nieskonczonego stringu
-
-	//znalezc pierwsza litere stringu
 	firstLetter := s[0]
 	fullString := ContactString(s, n)
 	var counts int64
-	//ile razy powtarza sie pierwsza litera§
+
 	for _, v := range fullString {
 		if string(v) == string(firstLetter) {
 			counts++
 		}
 	}
-
-	//fullString := ContactString(s, int(n))
 
 	return counts
 }
